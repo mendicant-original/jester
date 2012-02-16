8110a2320ff0cd5c18833261c9024c2026b5e6b6
Mayank Kohaley <mayank.kohaley@gmail.com>
2012-02-17 04:04:30 +0530

    Added the integration tests for help and default case

---
+++ tests/integration_tests.rb
@@ -0,0 +1,19 @@
+require './helper'
+
+describe "Jester" do
+  let(:server) { Newman.new_test_server(Jester::Application) }
+  let(:mailer) { server.mailer }
+  
+  it "responds with a help document" do
+    mailer.deliver_message(:to => 'test@test.com', :subject => 'help')
+    server.tick
+    mailer.messages.first.subject.must_equal("How to use Jester")
+  end
+  
+  it "responds with failure message when the subject does not match" do
+    mailer.deliver_message(:to => 'test@test.com')
+    server.tick
+    mailer.messages.first.subject.must_equal("Sorry, didn't understand your request")
+  end
+  
+end
